7584ffadfc3f3e3a6fecbff25d42036f448c4f2f
huangxia2000 <524439092@qq.com>
2019-09-22 17:05:30 +0800

    no message

--- MyDemo/src/main/java/com.soft/entity/TT.java
+++
@@ -1,4 +0,0 @@
-package com.soft.entity;
-
-public class TT {
-}
--- MyDemo/src/main/java/com.soft/web/AdminlistHandler.java
+++ MyDemo/src/main/java/com.soft/web/AdminlistHandler.java
@@ -1,7 +1,6 @@
 package com.soft.web;
 
 import com.soft.biz.UserBiz;
-import com.soft.entity.TblAdmin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
